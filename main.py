b8fe809af43d804aca30bb6099908a2d9a703c1b
Zhaorui Li <lizhao@desktop-375>
2022-06-21 12:11:06 +0200

    add menu

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 import pygame
 import sys
 import random
+from math import sin,cos,pi
 
-speed = 10
+# parametre
+speed_player = 10
+speed_falling = 8
 ball_number = 5
+fps = 60
+duree = 30
+diffculty = ['easy','normal','hard']
+
+# config
 screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 screen_width = screen.get_width()
 screen_height = screen.get_height()
-fps = 60
-exp_time = 120
+time = 0
+ball_type = 1
 
 class Ball(pygame.sprite.Sprite):
     # 定义构造函数
@@ -24,17 +32,17 @@ class Ball(pygame.sprite.Sprite):
         elif type == 1:
             self.image = pygame.image.load("Image/circle_green.png")
             self.speed_x = 0
-            self.speed_y = 8
+            self.speed_y = speed_falling
             self.image = pygame.transform.scale(self.image, (74, 74))
         elif type == 2:
             self.image = pygame.image.load("Image/circle_pink.png")
             self.speed_x = 0
-            self.speed_y = 8
+            self.speed_y = speed_falling
             self.image = pygame.transform.scale(self.image, (74, 74))
         elif type == 3:  
             self.image = pygame.image.load("Image/circle_orange.png")  
             self.speed_x = 0
-            self.speed_y = 8
+            self.speed_y = speed_falling
             self.image = pygame.transform.scale(self.image, (74, 74))
         # 加载图片并且缩小
         self.rect = self.image.get_rect()
@@ -65,17 +73,17 @@ def ready(word):
                 sys.exit()
 
 def control_ball_bar():
-    global speed
+    global speed_player
     global step
     key_pressed_is = pygame.key.get_pressed()
     if key_pressed_is[pygame.K_LEFT] & (player_ball.rect.left > 0):
-        player_ball.rect.left -= speed
+        player_ball.rect.left -= speed_player
     if key_pressed_is[pygame.K_RIGHT] & (player_ball.rect.right < screen_width):
-        player_ball.rect.left += speed
-    if key_pressed_is[pygame.K_UP] & (abs(speed) < 5):
-        speed = (speed > 0) * (abs(speed) + 1)
-    if key_pressed_is[pygame.K_DOWN] & (abs(speed) > 1):
-        speed = (speed > 0) * (abs(speed) - 1)
+        player_ball.rect.left += speed_player
+    if key_pressed_is[pygame.K_UP]:
+        speed_player += 1
+    if key_pressed_is[pygame.K_DOWN] & (abs(speed_player) > 1):
+        speed_player -= 1
     if key_pressed_is[pygame.K_SPACE]:
         step -= 5
 
@@ -86,13 +94,13 @@ def draw_balls():
         if (ball.status == 0) and (ball.rect.top > ball.random_pos):   
             if ball.type == 1:
                 ball.speed_x = 0
-                ball.speed_y = 8
+                ball.speed_y = speed_falling
             elif (ball.type == 2) and (random.randint(0, 9) < 4):
-                ball.speed_x = 4 * random.randrange(-1,2,2)
-                ball.speed_y = 7
+                ball.speed_x = speed_falling * random.randrange(-1,2,2) * sin(pi/6)
+                ball.speed_y = speed_falling * cos(pi/6)
             elif (ball.type == 3) and (random.randint(0, 9) < 9):  
-                ball.speed_x = 4 * random.randrange(-1,2,2)
-                ball.speed_y = 7
+                ball.speed_x = speed_falling * random.randrange(-1,2,2) * sin(pi/6)
+                ball.speed_y = speed_falling * cos(pi/6)
             ball.status = 1
         ball.rect.left += ball.speed_x
         ball.rect.top += ball.speed_y
@@ -101,28 +109,29 @@ def draw_balls():
         if ball.rect.top < 0 or (ball.rect.bottom > screen_height-30):
             score += 1
             group_ball.remove(ball)
-            group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
+            group_ball.add(Ball(random.randint(1,ball_type), (random.randint(0, screen_width - 100), 0)))
         # 下落小球 碰撞边界变向
         crash_result = pygame.sprite.collide_mask(player_ball, ball)
         if crash_result:
-            print("score:%d" % score)
-            show_score()
-            group_ball.empty()
-            score = 0
-            step = 0
-            ball_number = 5
-            ready("Crash!!Press enter to restart or esc to quit")
-            for i in range(ball_number):
-                group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
+            score -= 10
+            group_ball.remove(ball)
+            group_ball.add(Ball(random.randint(1,ball_type), (random.randint(0, screen_width - 100), 0)))
+            # print("score:%d" % score)
+            # show_score()
+            # group_ball.empty()
+            # score = 0
+            # step = 0
+            # ball_number = 5
+            # ready("Crash!!Press enter to restart or esc to quit")
+            # for i in range(ball_number):
+            #     group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
         # 碰撞检测
 
 def init():
-    global player_ball
-    global group_ball
     global clock
     global add_ball
     global get_score
-    global game_end
+    global tick1s
     global score
     global step
     global f
@@ -138,24 +147,14 @@ def init():
     # 填充屏幕颜色
     pygame.display.set_caption('Jeu de boule')
     # 设置窗口标题
-    location = (screen_width / 2, screen_height * 0.8)
-    player_ball = Ball(0, location)
-    # 创建玩家的球
-    group_ball = pygame.sprite.Group()
-    # 向组内添加一个精灵
-    for i in range(ball_number):
-        group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
-    # 创建下落的球组
     clock = pygame.time.Clock()
     # create clock
-    add_ball = pygame.USEREVENT + 1
-    pygame.time.set_timer(add_ball,5 * 1000)
-    # add balls while time pass
     get_score = pygame.USEREVENT + 2
     pygame.time.set_timer(get_score,300)
     # get score while maintain bar in 0.7-0.9
-    game_end = pygame.USEREVENT + 3
-    pygame.time.set_timer(game_end,exp_time * 1000)
+    tick1s = pygame.USEREVENT + 3
+    pygame.time.set_timer(tick1s, 1000)
+    # set timer
 
 def show_score():
     my_font = pygame.font.SysFont('arial', 50)
@@ -165,6 +164,17 @@ def show_score():
     textRect = text.get_rect()
     # 设置显示对象居中
     textRect.center = (300, 800)
+    screen.blit(text, textRect)
+    # 得分显示
+
+def show_time():
+    my_font = pygame.font.SysFont('arial', 50)
+    dy = f"time:{time}"
+    text = my_font.render(dy, True, (0, 0, 0))
+    # 获得显示对象的 rect区域大小
+    textRect = text.get_rect()
+    # 设置显示对象居中
+    textRect.center = (300, 300)
     screen.blit(text, textRect)
     # 得分显示
 
@@ -178,6 +188,7 @@ def flash():
         f.write("position(%d,%d)\n"%(ball.rect.left,ball.rect.top))
     f.write("0,0,0,0,0\n")
     show_score()
+    show_time()
     pygame.display.flip()
     # 刷新界面显示
 
@@ -208,10 +219,84 @@ def draw_bar():
     #         group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
     # test bar 
 
+def menu():
+    global ball_type
+    pygame.mouse.set_visible(True)
+    my_font = pygame.font.SysFont('arial', 80)
+    textRect = []
+    for i in range(0,3):
+        word = diffculty[i]
+        text = my_font.render(word, True, (0, 0, 0))
+        textRect.append(text.get_rect())
+        textRect[i].center = ((i+1)*screen_width/4, 500)
+        screen.blit(text, textRect[i])
+        # show "easy"
+    
+    word = "diffculty"
+    text = my_font.render(word, True, (0, 0, 0))
+    # 获得显示对象的 rect区域大小
+    textRect_diff = text.get_rect()
+    # 设置显示对象居中
+    textRect_diff.center = (screen_width/2, 200)
+    screen.blit(text, textRect_diff)
+    # show diffculty
+    pygame.display.flip()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for rect in textRect:
+                    if rect.collidepoint(event.pos):
+                        ball_type = 1 + textRect.index(rect)
+                        screen.fill((156, 156, 156))
+                        return
+            if event.type == pygame.QUIT:
+                f.close()
+                pygame.quit()
+                sys.exit()
+
+def choose_nb_ball():
+    global ball_number
+    my_font = pygame.font.SysFont('arial', 80)
+    textRect = []
+    for i in range(5,10):
+        word = str(i)
+        text = my_font.render(word, True, (0, 0, 0))
+        # 获得显示对象的 rect区域大小
+        textRect.append(text.get_rect())
+        # 设置显示对象居中
+        textRect[i-5].center = ((i-4)*screen_width/6, 500)
+        screen.blit(text, textRect[i-5])
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for rect in textRect:
+                    if rect.collidepoint(event.pos):
+                        ball_number = 5 + textRect.index(rect)
+                        screen.fill((156, 156, 156))
+                        return
+            if event.type == pygame.QUIT:
+                f.close()
+                pygame.quit()
+                sys.exit()
+
+
 if __name__ == '__main__':   
     init()
+    menu()
+    choose_nb_ball()
+    pygame.mouse.set_visible(False)
     ready("Are You Ready? Press enter to begin")
     # ready function
+    location = (screen_width / 2, screen_height * 0.8)
+    player_ball = Ball(0, location)
+    # 创建玩家的球
+    group_ball = pygame.sprite.Group()
+    # 向组内添加一个精灵
+    for i in range(ball_number):
+        group_ball.add(Ball(random.randint(1,ball_type), (random.randint(0, screen_width - 100), 0)))
+        # 创建下落的球组
     while True:
         clock.tick(fps)
         # 游戏主循环
@@ -220,24 +305,23 @@ if __name__ == '__main__':
                 f.close()
                 pygame.quit()
                 sys.exit()
-            if (event.type == add_ball) and (ball_number <= 8):
-                ball_number += 1
-                group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
-            # add balls while time pass
             if event.type == get_score:
                 if (step > screen_width*0.2)and (step < screen_width*0.4):
                     score += 1
                 # add score while time pass and bar is in 0.7-0.9
-            if event.type == game_end:
-                print("score:%d" % score)
-                show_score()
-                group_ball.empty()
-                score = 0
-                step = 0
-                ball_number = 5
-                ready("Time's up! Press enter to restart")
-                for i in range(ball_number):
-                    group_ball.add(Ball(random.randint(1,3), (random.randint(0, screen_width - 100), 0)))
+            if event.type == tick1s:
+                time += 1
+                if(time >= duree):
+                    time = 0
+                    screen.fill((156, 156, 156))
+                    show_score()
+                    group_ball.empty()
+                    score = 0
+                    step = 0
+                    ball_number = 5
+                    ready("Time's up!!Press enter to restart or esc to quit")
+                    for i in range(ball_number):
+                        group_ball.add(Ball(random.randint(1,ball_type), (random.randint(0, screen_width - 100), 0)))
         control_ball_bar()
         # 移动玩家小球
         flash()
